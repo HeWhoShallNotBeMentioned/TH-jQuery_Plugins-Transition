f042f600009f5a0092bf6d832e1e4221d3bb020a
Chris Underwood <cunderwoodmn@gmail.com>
2016-10-22 19:11:27 -0700

    add object literal for customizations of animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-$(".animsition").animsition();
+$(".animsition").animsition({
+  inClass: 'fade-in-up-lg',
+  outClass: 'flip-out-x',
+  linkElement: 'header a',
+  inDuration: 1000,
+  outDuration: 600
+});
